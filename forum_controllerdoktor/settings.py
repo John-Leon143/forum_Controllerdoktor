3ce2a1f07fba4a4bcbd44975da9cc954e6f7c0fb
John-Leon143 <64746626+John-Leon143@users.noreply.github.com>
2025-11-03 21:33:07 +0100

    habe was vergessen hinzuzufügen

--- forum_controllerdoktor/settings.py
+++ forum_controllerdoktor/settings.py
@@ -75,6 +75,9 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    
+    'machina.apps.forum_permission.middleware.ForumPermissionMiddleware',
+    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
