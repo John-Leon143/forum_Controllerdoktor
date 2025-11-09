0e349e244e233ff670e47dd8ada24c75debcf49d
John-Leon143 <64746626+John-Leon143@users.noreply.github.com>
2025-11-09 19:49:16 +0100

    überarbeitung

--- Forum_home/sitemaps.py
+++ Forum_home/sitemaps.py
@@ -18,7 +18,8 @@ class ForumSitemap(Sitemap):
         return obj.last_post_on if obj.last_post_on else obj.updated
 
     def location(self, obj):
-        return obj.get_absolute_url()
+        # Verwende reverse() statt get_absolute_url()
+        return reverse('forum:forum', kwargs={'slug': obj.slug, 'pk': obj.pk})
 
 
 class TopicSitemap(Sitemap):
@@ -31,13 +32,19 @@ class TopicSitemap(Sitemap):
         return Topic.objects.filter(
             approved=True,
             status=Topic.TOPIC_POST
-        ).select_related('forum').order_by('-last_post_on')[:5000]  # Limit für Performance
+        ).select_related('forum').order_by('-last_post_on')[:5000]
 
     def lastmod(self, obj):
         return obj.updated
 
     def location(self, obj):
-        return obj.get_absolute_url()
+        # Verwende reverse() mit korrekten Parametern
+        return reverse('forum_conversation:topic', kwargs={
+            'forum_slug': obj.forum.slug,
+            'forum_pk': obj.forum.pk,
+            'slug': obj.slug,
+            'pk': obj.pk
+        })
 
 
 class RecentPostsSitemap(Sitemap):
@@ -55,4 +62,10 @@ class RecentPostsSitemap(Sitemap):
         return obj.updated
 
     def location(self, obj):
-        return obj.get_absolute_url()
+        # Link zum Topic (Posts haben meist keine eigene URL)
+        return reverse('forum_conversation:topic', kwargs={
+            'forum_slug': obj.topic.forum.slug,
+            'forum_pk': obj.topic.forum.pk,
+            'slug': obj.topic.slug,
+            'pk': obj.topic.pk
+        })
